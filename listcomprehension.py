5b2a4a33a626ef71840f9a961cfb2f178d655aee
Henry Torres <htorrespo@users.noreply.github.com>
2018-09-17 16:25:59 -0500

    Create listcomprehension.py

---
+++ listcomprehension.py
@@ -0,0 +1,12 @@
+# A list comprehension is a syntactic construct available in some programming languages 
+# for creating a list based on existing lists.
+
+# The basic syntax is
+# [ expression for item in list if conditional ]
+
+# This is equivalent to:
+# for item in list:
+#    if conditional:
+#        expression
+
+# new_list = [expression(i) for i in old_list if filter(i)]
